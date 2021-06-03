981d91b70aa1f4ba998253b512b236935f03756e
lluvia365 <lluvia365@gmail.com>
2021-06-03 15:52:42 +0900

    1

--- script/index.js
+++ script/index.js
@@ -1,4 +1,9 @@
+
 $(function(){
+    
+    $('.slide').eq(0).fadeIn();   //첫 번째 슬라이드 이미지 보이기
+    
+    
     /* 내비 메인에 마우스 닿으면 서브메뉴 보이게 */
     $('.main').mouseenter(function(){
         $('.subMenu, #subBack').stop().slideDown(300);
@@ -7,6 +12,7 @@ $(function(){
     $('.main').mouseleave(function(){
         $('.subMenu, #subBack').stop().slideUp(200);    
     });
+    
     
     
     /* 공지사항 첫번째 클릭하면 모달창 보이게 */
@@ -19,3 +25,20 @@ $(function(){
     });
     
 });
+
+
+var slideNo=0;
+
+function slide(){
+    slideNo++;
+    if(slideNo>2) slideNo=0;
+    
+    $('.slide').fadeOut();
+    $('.slide').eq(slideNo).fadeIn();
+}
+
+function playSlide(){
+    setInterval('slide()',3000);
+}
+
+
